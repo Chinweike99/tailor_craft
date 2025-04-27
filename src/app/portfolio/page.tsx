69dde9379508a703b5dbe3de7c803eecac1308ec
Akwolu Innocent Chinweike <135303273+Chinweike99@users.noreply.github.com>
2025-04-27 11:43:46 +0100

    main (#16)
    * test
    
    Co-authored-by: Akwolu Innocent Chinweike <135303273+Chinweike99@users.noreply.github.com>
    Co-authored-by: Chinweike99 <innocentakwolu1@gmail.com>
    
    * Merge pull request #1 from Chinweike99/dev (#2)
    
    * testing build (#4)
    
    * Feat/booking form (#5)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * Feat/booking form (#6)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * Feat/booking form (#7)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * Feat/booking form (#8)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * Feat/booking form (#9)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * Feat/booking form (#10)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * Feat/booking form (#11)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * feat: navbar & footer
    
    * Feat/booking form (#12)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * feat: navbar & footer
    
    * reuseable input component
    
    * Feat/booking form (#13)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * feat: navbar & footer
    
    * reuseable input component
    
    * about page
    
    * Feat/booking form (#14)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * feat: navbar & footer
    
    * reuseable input component
    
    * about page
    
    * portfolio, contact, booking, testimonials pages
    
    * Feat/booking form (#15)
    
    * testing build
    
    * type file
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * modified Button.tsx
    
    * portfolio section
    
    * updated button.tsx
    
    * bookform component
    
    * testimonial carousel
    
    * fix img, tailoring images
    
    * fix img, tailoring images
    
    * feat: navbar & footer
    
    * reuseable input component
    
    * about page
    
    * portfolio, contact, booking, testimonials pages
    
    * added images
    
    ---------
    
    Co-authored-by: Akwolu Innocent Chinweike <130150968+Famouz99@users.noreply.github.com>

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -102,7 +102,11 @@ export default function PortfolioPage() {
     : portfolioItems;
 
   return (
+<<<<<<< HEAD
     <main className="container mx-auto px-4 py-16 max-w-6xl">
+=======
+    <main className="pt-24 pb-12">
+>>>>>>> 7ea4f7e (main (#16))
       {/* Hero Section */}
       <section className="container mx-auto px-4 mb-16">
         <motion.div 
