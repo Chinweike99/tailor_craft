e0c90ac783a6b88726d1f16f53916ba9fa48fb9e
Chinweike99 <innocentakwolu1@gmail.com>
2025-05-02 18:33:40 +0100

    fix: width inconsistencies

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
         isScrolled ? 'bg-white/80 dark:bg-gray-900/10 backdrop-blur-md shadow-md py-2' : 'bg-transparent py-4'
       }`}
     >
-        <div className='max-w-[1440px] w-full '>
+        <div className=' w-full '>
       <div className="container mx-auto flex justify-between items-center">
         {/* <Link href="/" className="font-bold text-xl md:text-2xl">
           <span className="text-primary">Bespoke</span>
